74353b13d24e20c4debfe1742ede30f93a26a86a
Kévin <kevin.lemairecolombel@gmail.com>
2022-06-20 17:02:40 +0200

    add rainbowkit

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-function App() {
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { chain, createClient, configureChains, WagmiConfig } from "wagmi";
+import { Connect } from "./Connect";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  // TODO: { alchemyId: process.env.ALCHEMY_ID }
+  [alchemyProvider(), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Super Block",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Connect />
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
-
-export default App;
